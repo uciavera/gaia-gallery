dde9ff5ee4a7828e018a85db64faa6bd32b48d91
Qudsy <ucisolahuddin@gmail.com>
2020-12-10 19:08:55 +0700

    add search feature

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Page from './components/page/Page';
-import Navbar from './components/navbar/Navbar';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Row, Container} from 'react-bootstrap'
+import {Row, Container} from 'react-bootstrap';
+import React, {useState} from 'react'
+import { FaTags } from 'react-icons/fa';
 
 function App() {
+  
   return (
     <Container>
       
-      <Navbar/>
-      
-      
-      <Page/>
-      
+      <Page />
     </Container>
     
   );
--- src/components/page/Page.jsx
+++ src/components/page/Page.jsx
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from 'react'
 import { FaCaretSquareLeft, FaCaretSquareRight } from 'react-icons/fa';
+import { Navbar, Form, Button, FormControl } from 'react-bootstrap'
 
 import axios from 'axios'
 
 export default function Page() {
-
+    const [tag, setTag] = useState(null);
     const [image, setImage] = useState([])
+    const [image2, setImage2] = useState([])
     const [currentClinic, setCurrentClinic] = useState(1)
+    const [showImage, setShowImage] = useState(true)
+    const [showSearch, setShowSearch] = useState(false)
     const maxItem = 4
 
     var first = (currentClinic * maxItem) - maxItem
@@ -20,18 +24,32 @@ export default function Page() {
         } else if (direction === 'back') {
             setCurrentClinic(currentClinic - 1)
         }
+    };
+     const search=()=>{
+        
+        searcha();
+        setShowSearch(true);
+        setShowImage(false)
+    };
+    const searcha=()=>{
+        axios.get(
+            `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=ed9aeed6915893540672706939818b0c&tags=${tag}&format=json&nojsoncallback=1`
+            )
+            .then(response => setImage2(response.data.photos.photo))
     }
-
 
     useEffect(() => {
 
-        axios.get(
-            'https://www.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=ed9aeed6915893540672706939818b0c&gallery_id=72157647915728815&format=json&nojsoncallback=1'
-        )
-            .then(response => setImage(response.data.photos.photo))
+    axios.get(
+    'https://www.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=ed9aeed6915893540672706939818b0c&gallery_id=72157647915728815&format=json&nojsoncallback=1'
+)
+    .then(response => setImage(response.data.photos.photo))
+
 
     }, []);
     console.log(image);
+    console.log(tag);
+    console.log(image2);
 
     const imageList = image && image.slice(first, last).map((item, index) => {
         return (
@@ -47,19 +65,45 @@ export default function Page() {
         );
 
     });
-    const maxClinic = image && image.length - 1;
+    const imageSearch = image2 && image2.slice(first, last).map((item, index) => {
+        return (
+            <div className="col-md-3 col-sm-6" key={index}>
+                <div className="card" style={{ width: '20em', height: '20rem', margin: '5px auto', overflowX: 'hidden' }}>
 
+                    <img src={`https://farm${item.farm}.staticflickr.com/${item.server}/${item.id}_${item.secret}.jpg`}
+                        alt="..." style={{ width: '20em', height: '20rem' }}></img>
+
+                </div>
+
+            </div>
+        );
+
+    });
+    const maxClinic = image && image.length - 1;
+    
     return (
         <div>
             <div className="row mt-3">
-                {imageList}
+                <Navbar bg="light" expand="lg" style={{ margin: '5px auto' }}>
+                    <Navbar.Brand href="#home" style={{}}>GAIA Gallery</Navbar.Brand>
+
+                    <Form inline style={{ width: '50rem', margin: '8px auto' }}>
+                        <FormControl type="text" placeholder="Search" className="mr-sm-2"
+                            style={{ width: '32rem', margin: ' auto' }} onChange={e => setTag(e.target.value)} />
+                        <Button style={{ margin: 'auto 8px' }} variant="outline-success"  onClick={()=>search()}>Search</Button>
+                    </Form>
+                </Navbar>
             </div>
+            {showImage ? <div className="row mt-3">
+                {imageList}
+            </div> : null}
+            {showSearch ? <div className="row mt-3">
+                {imageSearch}
+            </div> : null}
 
-
-
-            <div className="row mt-3" style={{ width: '5rem', margin: '8px auto'}}>
+            <div className="row mt-3" style={{ width: '5rem', margin: '8px auto' }}>
                 {currentClinic > 1 ?
-                    <span 
+                    <span
                         onClick={() => changePage('back')}
                     ><FaCaretSquareLeft /></span>
                     : <span
@@ -68,7 +112,7 @@ export default function Page() {
                 {maxClinic > (currentClinic * maxItem) ?
                     <span
                         onClick={() => changePage('next')}
-                    ><FaCaretSquareRight  /></span>
+                    ><FaCaretSquareRight /></span>
                     : <span
 
                     ><FaCaretSquareRight /></span>
